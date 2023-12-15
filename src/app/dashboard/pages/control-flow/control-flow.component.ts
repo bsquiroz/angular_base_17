60b6190992a309a93c69577a0e38773e09c74fd8
bsquiroz <bemq1605@gmail.com>
2023-12-14 20:55:43 -0500

    add control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@app/shared/title/title.component';
+
+type Grade = 'A' | 'B' | 'C';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './control-flow.component.html',
 })
-export default class ControlFlowComponent {}
+export default class ControlFlowComponent {
+  public showContent = signal(false);
+  public grade = signal<Grade>('C');
+  public frameworks = signal([
+    'angular',
+    'astro',
+    'vue',
+    'next',
+    'react',
+    'svelte',
+  ]);
+
+  public toggleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './title.component.html',
+  template: `<h1
+    [class]="
+      withShadow
+        ? 'shadow mb-5 shadow-blue-200 p-5 text-3xl font-bold'
+        : 'mb-5 p-5 text-3xl font-bold'
+    "
+  >
+    {{ title }} - {{ withShadow }}
+  </h1>`,
 })
-export class TitleComponent {}
+export class TitleComponent {
+  @Input({ required: true }) title: string = '';
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+}
