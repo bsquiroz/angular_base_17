e040d26e87965be70ce678e63885d3ddef54e80b
bsquiroz <bemq1605@gmail.com>
2023-12-14 19:06:43 -0500

    finish config sidemenu

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path && !route.path?.includes(':'));
+
+  constructor() {}
+}
